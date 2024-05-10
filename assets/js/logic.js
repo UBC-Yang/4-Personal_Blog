711b40776187a3d7beae5223b8a60575eee09c83
Shu Yang <shuyang@Thomass-MacBook-Pro.local>
2024-05-10 13:00:44 -0600

    ready for deployment

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,22 +2,23 @@
 //THEN I am presented with a list of blog posts that are pulled from localStorage.
 
 const storedPost = localStorage.getItem('blogPosts');
+const posts = JSON.parse(storedPost);
 
-for (i = 0; i < storedPost.length; i++) {
-    if (storedPost) {
-        const post = JSON.parse(storedPost);
+
+    if (posts) {
+        for (i = 0; i < posts.length; i++) {
         const mainContentContainer = document.getElementById('mainContent');
         const postElement = document.createElement('div');
+        postElement.setAttribute('class', 'blog');
         postElement.innerHTML = `
-            <h2>${post.username}</h2>
-            <h2>${post.title}</h2>
-            <p>${post.content}</p>
+            <h2>Title: ${posts[i].title}</h2>
+            <p>${posts[i].content}</p>
+            <h2>User: ${posts[i].username}</h2>
             `;
         mainContentContainer.appendChild(postElement)
+        }
     }
-}
 
-console.log(storedPost)
 
 
 
